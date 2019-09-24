75e5564b8ee51809a2e605e053d855b5539829c7
Dominique Saripapazoglou <dsaripap@f0r4s15.codam.nl>
2019-09-24 23:38:16 +0200

    Trying to figure out the algorithm of dividing a string

--- ft_float.c
+++ ft_float.c
@@ -12,13 +12,13 @@
 
 #include "ft_printf.h"
 
-void            print_bin(uint64_t mant)
+void			print_bin(uint64_t mant)
 {
-    int			bit;
-    int			pos;
+	int			bit;
+	int			pos;
 
-    pos = 0;
-    while (pos <= 64)
+	pos = 0;
+	while (pos <= 64)
 	{
 		bit = mant << pos;
 		if (bit & 1)
@@ -29,50 +29,55 @@ void            print_bin(uint64_t mant)
 	}
 }
 
-void            str_half(char *fr)
+void			str_half(char *fr)
 {
-    double      temp;
+	int			temp;
+	int			len;
 
-    temp = 0;
-    if (fr[0] == '1')
-        fr[0] = '0';
-    if (fr[201] == '0')
-        fr[201] = '5';
-    else
-    {
-        temp = ft_atoi(&fr[201]);
-        temp = temp / 2;
-        printf("\ntemp = %f\n", temp);
-    }
+	len = ft_strlen(fr);
+	printf("len = %d\n", len);
+	if (len == 3)
+		temp = 10;
+	if (fr[0] == '1')
+		fr[0] = '0';
+	temp = (temp * 10) / 2;
+	printf("temp = %d\n", temp);
+	fr[2] = temp;
 }
 
-void            str_add(short *pr, char *fr)
+void			str_add(short *pr, char *fr)
 {
-    pr[0] = '1';
-    fr[0] = '0';
+	pr[0] = '1';
+	fr[0] = '0';
 }
 
-void            ft_ftoa(va_list argptr, t_format *t_flags)
+void			ft_ftoa(va_list argptr, t_format *t_flags)
 {
-    t_float     fl_num;
-    char		fraction[400];
+	t_float		fl_num;
+	char		fraction[400];
 	short		product[10000];
+	int			temp;
 
-    printf("\nflag = %d\n", (*t_flags).flags);
-    fraction[0] = '1';
-    fraction[1] = '.';
-    fraction[201] = '0';
-    product[5000] = '0';
-    product[5001] = '.';
-    product[5002] = '0';
-    fl_num.f_num = va_arg(argptr, long double);
-    while (fl_num.mantissa > 0)
-    {
-        printf("\nmantissa = %llu\n", fl_num.mantissa);
-        print_bin(fl_num.mantissa);
-        if (fl_num.mantissa & 1)
-            str_add(product, fraction);
-        str_half(fraction);
-        fl_num.mantissa = fl_num.mantissa << 1;
-    }
+	printf("\nflag = %d\n", (*t_flags).flags);
+	fraction[0] = '1';
+	fraction[1] = '.';
+	fraction[2] = '0';
+	product[5000] = '0';
+	product[5001] = '.';
+	product[5002] = '0';
+	fl_num.f_num = va_arg(argptr, long double);
+	// while (fl_num.mantissa > 0)
+	temp = 0;
+	while (temp < 3)
+	{
+		printf("\nmantissa = %llu\n", fl_num.mantissa);
+		print_bin(fl_num.mantissa);
+		if (fl_num.mantissa & 1)
+			str_add(product, fraction);
+		printf("\nfraction = %c%c%c\n", fraction[0], fraction[1], fraction[2]);
+		str_half(fraction);
+		printf("fraction = %c%c%c\n", fraction[0], fraction[1], fraction[2]);
+		fl_num.mantissa = fl_num.mantissa << 1;
+		temp++;
+	}
 }
